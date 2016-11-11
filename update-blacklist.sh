16597c6370001f48dc0a53bf00694599f016ac17
Laurent Monin <github@norz.org>
2016-11-11 17:50:54 +0100

    Whitelists, for now just add nomatch to our ipset

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -81,6 +81,27 @@ fi
 # sort -nu does not work as expected
 cat "$IP_BLACKLIST_REMOTE" "$IP_BLACKLIST_LOCAL" | sed -r -e '/^(10\.|127\.|172\.16\.|192\.168\.)/d'|sort -V|sort -mu >| "$IP_BLACKLIST"
 
+# local white lists
+if [[ ! -e "$IP_WHITELIST_LOCAL" ]]; then
+	touch "$IP_WHITELIST_LOCAL"
+	IP_WHITELIST_LOCAL_TMP="$MYTMPDIR/ip_whitelist_local_tmp"
+	for i in "${WHITELISTS_LOCAL[@]}"
+	do
+		FILE="$IP_BLACKLIST_DIR/$i"
+		if [[ -e "$FILE" ]]; then
+			[[ "$VERBOSE" == yes ]] && echo "Whitelisting IPs from $FILE"
+			grep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$FILE" >> "$IP_WHITELIST_LOCAL_TMP"
+		else
+			echo >&2 -e "\nError: WHITELIST_LOCAL: no such file: $FILE"
+			exit 1
+		fi
+	done
+	sort -V "$IP_WHITELIST_LOCAL_TMP"|sort -n|sort -mu >| "$IP_WHITELIST_LOCAL"
+	rm -f "$IP_WHITELIST_LOCAL_TMP"
+fi
+
+IP_WHITELIST="$IP_WHITELIST_LOCAL"
+
 # family = inet for IPv4 only
 IPSET_BLACKLIST_OPTIONS="-exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}"
 IPSET_BLACKLIST_CREATE="create $IPSET_BLACKLIST_NAME $IPSET_BLACKLIST_OPTIONS"
@@ -89,7 +110,14 @@ cat >| "$IP_BLACKLIST_RESTORE" <<EOF
 $IPSET_TMP_BLACKLIST_CREATE
 $IPSET_BLACKLIST_CREATE
 flush $IPSET_TMP_BLACKLIST_NAME
+add $IPSET_TMP_BLACKLIST_NAME 10.0.0.0/8 nomatch
+add $IPSET_TMP_BLACKLIST_NAME 127.0.0.0/8 nomatch
+add $IPSET_TMP_BLACKLIST_NAME 172.16.0.0/12 nomatch
+add $IPSET_TMP_BLACKLIST_NAME 192.168.0.0/16 nomatch
 EOF
+
+sed -rn -e '/^#|^$/d' \
+    -e "s/^([0-9./]+).*/add $IPSET_TMP_BLACKLIST_NAME \1 nomatch/p" "$IP_WHITELIST" >> "$IP_BLACKLIST_RESTORE"
 
 # can be IPv4 including netmask notation
 # IPv6 ? -e "s/^([0-9a-f:./]+).*/add $IPSET_TMP_BLACKLIST_NAME \1/p" \ IPv6
