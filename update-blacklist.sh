7256a74d9e42924ab1b0a516dde89b9e8172ec97
Laurent Monin <github@norz.org>
2016-11-11 16:06:16 +0100

    Heavily modify the script to suit our needs
    - trap exit and ensure temporary files are removed
    - add a list of local blacklist files
    - only build new lists if ip-blacklist-local.list
      or ip-blacklist-remote.list don't exist.
      So it is possible to blacklist IPs from local files without downloading remote ones.
    - add ONLY_CREATE_LIST option (default: yes) so the script doesn't run any iptables or ipset by itself
    - set configuration directory through IP_BLACKLIST_DIR

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+trap 'rm -rf "$MYTMPDIR"' EXIT
+
 # usage update-blacklist.sh <configuration file>
 # eg: update-blacklist.sh /etc/ipset-blacklist/ipset-blacklist.conf
 if [[ -z "$1" ]]; then
@@ -12,8 +14,11 @@ if ! source "$1"; then
     exit 1
 fi
 
-if ! which curl egrep grep ipset iptables sed sort wc &> /dev/null; then
-    echo >&2 "Error: searching PATH fails to find executables among: curl egrep grep ipset iptables sed sort wc"
+VERBOSE=${VERBOSE:-no}
+
+NEEDED_COMMANDS="curl egrep grep sed sort wc"
+if ! which $NEEDED_COMMANDS &> /dev/null; then
+    echo >&2 "Error: searching PATH fails to find executables among: $NEEDED_COMMANDS"
     exit 1
 fi
 
@@ -32,57 +37,93 @@ if [ -f /etc/ip-blacklist-custom.conf ]; then
     exit 1
 fi
 
-# create the ipset if needed (or abort if does not exists and FORCE=no)
-if ! ipset list -n|command grep -q "$IPSET_BLACKLIST_NAME"; then
-    if [[ ${FORCE:-no} != yes ]]; then
-	echo >&2 "Error: ipset does not exist yet, add it using:"
-	echo >&2 "# ipset create $IPSET_BLACKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}"
-	exit 1
-    fi
-    if ! ipset create "$IPSET_BLACKLIST_NAME" -exist hash:net family inet hashsize "${HASHSIZE:-16384}" maxelem "${MAXELEM:-65536}"; then
-	echo >&2 "Error: while creating the initial ipset"
-	exit 1
-    fi
+MYTMPDIR=$(mktemp -d)
+
+if [[ ${ONLY_CREATE_LIST:-yes} != yes ]]; then
+	NEEDED_COMMANDS_EXTRA="ipset iptables"
+	if ! which $NEEDED_COMMANDS_EXTRA &> /dev/null; then
+		echo >&2 "Error: searching PATH fails to find executables among: $NEEDED_COMMANDS_EXTRA"
+		exit 1
+	fi
+
+	# create the ipset if needed (or abort if does not exists and FORCE=no)
+	if ! ipset list -n|command grep -q "$IPSET_BLACKLIST_NAME"; then
+		if [[ ${FORCE:-no} != yes ]]; then
+		echo >&2 "Error: ipset does not exist yet, add it using:"
+		echo >&2 "# ipset create $IPSET_BLACKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}"
+		exit 1
+		fi
+		if ! ipset create "$IPSET_BLACKLIST_NAME" -exist hash:net family inet hashsize "${HASHSIZE:-16384}" maxelem "${MAXELEM:-65536}"; then
+		echo >&2 "Error: while creating the initial ipset"
+		exit 1
+		fi
+	fi
+
+	# create the iptables binding if needed (or abort if does not exists and FORCE=no)
+	if ! iptables -vL INPUT|command grep -q "match-set $IPSET_BLACKLIST_NAME"; then
+		# we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
+		if [[ ${FORCE:-no} != yes ]]; then
+		echo >&2 "Error: iptables does not have the needed ipset INPUT rule, add it using:"
+		echo >&2 "# iptables -I INPUT ${IPTABLES_IPSET_RULE_NUMBER:-1} -m set --match-set $IPSET_BLACKLIST_NAME src -j DROP"
+		exit 1
+		fi
+		if ! iptables -I INPUT "${IPTABLES_IPSET_RULE_NUMBER:-1}" -m set --match-set "$IPSET_BLACKLIST_NAME" src -j DROP; then
+		echo >&2 "Error: while adding the --match-set ipset rule to iptables"
+		exit 1
+		fi
+	fi
+elif [[ "$VERBOSE" == yes ]]; then
+	echo "You'll need to run following commands:"
+	echo "ipset create $IPSET_BLACKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}"
+	echo "iptables -I INPUT ${IPTABLES_IPSET_RULE_NUMBER:-1} -m set --match-set $IPSET_BLACKLIST_NAME src -j DROP"
 fi
 
-# create the iptables binding if needed (or abort if does not exists and FORCE=no)
-if ! iptables -vL INPUT|command grep -q "match-set $IPSET_BLACKLIST_NAME"; then
-    # we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
-    if [[ ${FORCE:-no} != yes ]]; then
-	echo >&2 "Error: iptables does not have the needed ipset INPUT rule, add it using:"
-	echo >&2 "# iptables -I INPUT ${IPTABLES_IPSET_RULE_NUMBER:-1} -m set --match-set $IPSET_BLACKLIST_NAME src -j DROP"
-	exit 1
-    fi
-    if ! iptables -I INPUT "${IPTABLES_IPSET_RULE_NUMBER:-1}" -m set --match-set "$IPSET_BLACKLIST_NAME" src -j DROP; then
-	echo >&2 "Error: while adding the --match-set ipset rule to iptables"
-	exit 1
-    fi
+if [[ ! -e "$IP_BLACKLIST_REMOTE" ]]; then
+	IP_BLACKLIST_TMP="$MYTMPDIR/ip_blacklist_tmp"
+	CURL_ERROR=false
+	for i in "${BLACKLISTS[@]}"
+	do
+		IP_TMP="$MYTMPDIR/ip_tmp"
+		let HTTP_RC=`curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o $IP_TMP -s -w "%{http_code}" "$i"`
+		if (( $HTTP_RC == 200 || $HTTP_RC == 302 || $HTTP_RC == 0 )); then # "0" because file:/// returns 000
+			command grep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" >> "$IP_BLACKLIST_TMP"
+		[[ "$VERBOSE" == yes ]] && echo "Adding IPs from $i"
+		else
+			CURL_ERROR=true
+			echo >&2 -e "\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
+		fi
+		rm -f "$IP_TMP"
+	done
+
+	if [[ "${CURL_ERROR}" = true && ${IGNORE_CURL_ERRORS:-yes} == no ]]; then
+		echo >&2 -e "\nError: curl returned an HTTP error code. Please fix or set IGNORE_CURL_ERRORS to yes"
+		exit 1
+	fi
+
+	# sort -nu does not work as expected
+	sed -r -e '/^(10\.|127\.|172\.16\.|192\.168\.)/d' "$IP_BLACKLIST_TMP"|sort -n|sort -mu >| "$IP_BLACKLIST_REMOTE"
+	rm -f "$IP_BLACKLIST_TMP"
 fi
 
-IP_BLACKLIST_TMP=$(mktemp)
-CURL_ERROR=false
-for i in "${BLACKLISTS[@]}"
-do
-    IP_TMP=$(mktemp)
-    let HTTP_RC=`curl -L -A "blacklist-update/script/github" --connect-timeout 10 --max-time 10 -o $IP_TMP -s -w "%{http_code}" "$i"`
-    if (( $HTTP_RC == 200 || $HTTP_RC == 302 || $HTTP_RC == 0 )); then # "0" because file:/// returns 000
-        command grep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" >> "$IP_BLACKLIST_TMP"
-	[[ ${VERBOSE:-yes} == yes ]] && echo -n "."
-    else
-        CURL_ERROR=true
-        echo >&2 -e "\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
-    fi
-    rm -f "$IP_TMP"
-done
-
-if [[ "${CURL_ERROR}" = true && ${IGNORE_CURL_ERRORS:-yes} == no ]]; then
-    echo >&2 -e "\nError: curl returned an HTTP error code. Please fix or set IGNORE_CURL_ERRORS to yes"
-    exit 1
+if [[ ! -e "$IP_BLACKLIST_LOCAL" ]]; then
+	IP_BLACKLIST_LOCAL_TMP="$MYTMPDIR/ip_blacklist_local_tmp"
+	for i in "${BLACKLISTS_LOCAL[@]}"
+	do
+		FILE="$IP_BLACKLIST_DIR/$i"
+		if [[ -e "$FILE" ]]; then
+			[[ "$VERBOSE" == yes ]] && echo "Adding IPs from $FILE"
+			grep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$FILE" >> "$IP_BLACKLIST_LOCAL_TMP"
+		else
+			echo >&2 -e "\nError: BLACKLIST_LOCAL: no such file: $FILE"
+			exit 1
+		fi
+	done
+	sed -r -e '/^(10\.|127\.|172\.16\.|192\.168\.)/d' "$IP_BLACKLIST_LOCAL_TMP"|sort -n|sort -mu >| "$IP_BLACKLIST_LOCAL"
+	rm -f "$IP_BLACKLIST_LOCAL_TMP"
 fi
 
 # sort -nu does not work as expected
-sed -r -e '/^(10\.|127\.|172\.16\.|192\.168\.)/d' "$IP_BLACKLIST_TMP"|sort -n|sort -mu >| "$IP_BLACKLIST"
-rm -f "$IP_BLACKLIST_TMP"
+cat "$IP_BLACKLIST_REMOTE" "$IP_BLACKLIST_LOCAL" | sed -r -e '/^(10\.|127\.|172\.16\.|192\.168\.)/d'|sort -V|sort -mu >| "$IP_BLACKLIST"
 
 # family = inet for IPv4 only
 cat >| "$IP_BLACKLIST_RESTORE" <<EOF
@@ -101,9 +142,13 @@ swap $IPSET_BLACKLIST_NAME $IPSET_TMP_BLACKLIST_NAME
 destroy $IPSET_TMP_BLACKLIST_NAME
 EOF
 
-ipset -file  "$IP_BLACKLIST_RESTORE" restore
+if [[ ${ONLY_CREATE_LIST:-yes} != yes ]]; then
+	ipset -file  "$IP_BLACKLIST_RESTORE" restore
+elif [[ "$VERBOSE" == yes ]]; then
+	echo "ipset -file  "$IP_BLACKLIST_RESTORE" restore"
+fi
 
-if [[ ${VERBOSE:-no} == yes ]]; then
+if [[ "$VERBOSE" == yes ]]; then
     echo
     echo "Number of blacklisted IP/networks found: `wc -l $IP_BLACKLIST | cut -d' ' -f1`"
 fi
